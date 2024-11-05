734176143d282241e56c5a7bda5bb49028ff73d5
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-11-04 22:11:58 -0300

    feat(service): update token service interface with user token payload dto

--- src/Application/Interfaces/ITokenService.cs
+++ src/Application/Interfaces/ITokenService.cs
@@ -1,11 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
+using UserAuth.API.DTOs;
 using UserAuth.Domain.Entities;
 
 namespace UserAuth.Application.Interfaces
 {
     public interface ITokenService
     {
-        public string GenerateToken(User user);
+        public string GenerateToken(UserTokenPayloadDTO userTokenPayloadDTO);
 
         public string RefreshToken();
     }
