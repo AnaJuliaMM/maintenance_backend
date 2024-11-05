30edd64acc3c652398e27436531e87306c50aad2
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-11-04 22:14:01 -0300

    feat: implement automapper to transform user entity into user token payload dto

--- src/Application/Services/AuthService.cs
+++ src/Application/Services/AuthService.cs
@@ -1,3 +1,4 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using UserAuth.API.DTOs;
 using UserAuth.Application.Helpers;
@@ -10,18 +11,23 @@ namespace UserAuth.Application.Services
     public class AuthService : IAuthService
     {
         private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
 
-        public AuthService(IUserRepository userRepository)
+
+        public AuthService(IUserRepository userRepository, IMapper mapper)
         {
             _userRepository = userRepository;
+            _mapper = mapper;
+
         }
 
-        public async Task<User?> LoginUser(UserLoginDTO userLoginDTO)
+        public async Task<UserTokenPayloadDTO?> LoginUser(UserLoginDTO userLoginDTO)
         {
             var user = await _userRepository.FindUserByUsername(userLoginDTO.Username);
             if (user != null)
                 if (PasswordHelper.VerifyPassword(userLoginDTO.Password, user.Password))
-                    return user;
+                    return _mapper.Map<UserTokenPayloadDTO>(user);
+
                     
             return null;
         }
