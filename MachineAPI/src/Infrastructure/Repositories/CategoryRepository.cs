80b5d3dad20b697a7170b202c35223705c513b3b
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-11-05 22:41:59 -0300

    feat(repository): create category repository

---
+++ MachineAPI/src/Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,48 @@
+using Microsoft.EntityFrameworkCore;
+using MachineAPI.Domain.Entities;
+using MachineAPI.Domain.Interfaces;
+
+namespace MachineAPI.Infrastructure.Data
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Category>> GetAll()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        public async Task<Category?> GetById(int id)
+        {
+            return await _context.Categories.FindAsync(id);
+        }
+
+        public async Task Add(Category category)
+        {
+            await _context.Categories.AddAsync(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Update(Category category)
+        {
+            _context.Categories.Update(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(int id)
+        {
+            Category? category = await _context.Categories.FindAsync(id);
+            if (category != null)
+            {
+                _context.Categories.Remove(category);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
