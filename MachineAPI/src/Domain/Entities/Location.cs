e5e983c94625dff9e856fff5191388c14afc6c79
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-11-05 22:25:53 -0300

    feat(locationEntity): add a list of machines as attr

--- MachineAPI/src/Domain/Entities/Location.cs
+++ MachineAPI/src/Domain/Entities/Location.cs
@@ -5,5 +5,9 @@ namespace MachineAPI.Domain.Entities
         public required int Id { get; set; }
         public required string Name { get; set; }
         public string Description { get; set; } = string.Empty;
+
+        // A location have a list of machines
+        public List<Machine> Machines { get; set; } = new List<Machine>();
+
     }
 }
