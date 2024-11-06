1cc13690cc1f6d61526254477cc219b4be18eced
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-11-05 22:41:04 -0300

    feat(repository): confif getbyid return method as nullable

--- MachineAPI/src/Domain/Interfaces/ICategoryRepository.cs
+++ MachineAPI/src/Domain/Interfaces/ICategoryRepository.cs
@@ -5,7 +5,7 @@ namespace MachineAPI.Domain.Interfaces
     public interface ICategoryRepository
     {
         Task<IEnumerable<Category>> GetAll();
-        Task<Category> GetById(int id);
+        Task<Category?> GetById(int id);
         Task Add(Category category);
         Task Update(Category category);
         Task Delete(int id);
