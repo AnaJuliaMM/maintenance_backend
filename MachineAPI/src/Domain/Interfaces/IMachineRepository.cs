2e071ebbe7ef0d9764c18b4e0b9cc537437708a0
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-11-05 22:32:41 -0300

    feat: set getbyid return as nullable

--- MachineAPI/src/Domain/Interfaces/IMachineRepository.cs
+++ MachineAPI/src/Domain/Interfaces/IMachineRepository.cs
@@ -5,7 +5,7 @@ namespace MachineAPI.Domain.Interfaces
     public interface IMachineRepository
     {
         Task<IEnumerable<Machine>> GetAll();
-        Task<Machine> GetById(int id);
+        Task<Machine?> GetById(int id);
         Task Add(Machine machine);
         Task Update(Machine machine);
         Task Delete(int id);
