3d43124855ffe422b8265127b69a786ec362a987
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-11-05 21:46:27 -0300

    feat(interfaces): create category repository interface

---
+++ MachineAPI/src/Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+using MachineAPI.Domain.Entities;
+
+namespace MachineAPI.Domain.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetAll();
+        Task<Category> GetById(int id);
+        Task Add(Category category);
+        Task Update(Category category);
+        Task Delete(int id);
+    }
+}
