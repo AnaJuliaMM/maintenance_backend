cc56c69a308608350fb732d53d0d852f2b1cca0a
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-11-05 22:25:40 -0300

    feat(categoryEntity): add a list of machines as attr

--- MachineAPI/src/Domain/Entities/Category.cs
+++ MachineAPI/src/Domain/Entities/Category.cs
@@ -4,5 +4,8 @@ namespace MachineAPI.Domain.Entities
     {
         public required int Id { get; set; }
         public required string Label { get; set; }
+
+        // A Categry have a list of machines
+        public List<Machine> Machines { get; set; } = new List<Machine>();
     }
 }
