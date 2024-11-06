4976dc6178a8ea76ca3577b9faead5df02a37be6
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-11-05 22:26:42 -0300

    feat: create machine and location and category relationship

--- MachineAPI/src/Infrastructure/Data/ApplicationDbContext.cs
+++ MachineAPI/src/Infrastructure/Data/ApplicationDbContext.cs
@@ -9,6 +9,28 @@ namespace MachineAPI.Infrastructure.Data
             : base(options) { }
 
         public DbSet<Machine> Machines { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Type> Types { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {   
+
+            // Create Machine and Category 1:n relationship
+            modelBuilder.Entity<Machine>()
+                .HasOne(machine => machine.Category)
+                .WithMany(category => category.Machines)
+                .HasForeignKey(machine => machine.CategoryId);
+
+            // Create Machine and Location 1:n relationship
+            modelBuilder.Entity<Machine>()
+                .HasOne(machine => machine.Location)
+                .WithMany(location => location.Machines)
+                .HasForeignKey(machine => machine.LocationId);
+
+            // Persist relationships
+            base.OnModelCreating(modelBuilder);
+        }
+
     }
 
 }
