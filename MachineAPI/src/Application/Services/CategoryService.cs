1847829f743004936127a80e1d7ac698a8787227
Ana Julia M Martins <ana.martins_2004@outlook.com>
2024-11-24 19:09:46 -0300

    feat: implement create and update and delete methods in service

--- MachineAPI/src/Application/Services/CategoryService.cs
+++ MachineAPI/src/Application/Services/CategoryService.cs
@@ -1,32 +1,56 @@
 using AutoMapper;
+using MachineAPI.API.DTOs;
 using MachineAPI.Application.Interfaces;
 using MachineAPI.Domain.Entities;
 using MachineAPI.Domain.Interfaces;
-using MachineAPI.API.DTOs;
 
 namespace MachineAPI.Application.Services
 {
-public class CategoryService : ICategoryService
-{
-    private readonly ICategoryRepository _categoryRepository;
-    private readonly IMapper _mapper;
-
-    public CategoryService(ICategoryRepository categoryRepository, IMapper mapper)
+    public class CategoryService : ICategoryService
     {
-        _categoryRepository = categoryRepository;
-        _mapper = mapper;
-    }
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
 
-    public async Task<CategoryDTO?> GetById(int id)
-    {
-        Category? category = await _categoryRepository.GetById(id);
-        return category != null ? _mapper.Map<CategoryDTO>(category) : null;
-    }
+        public CategoryService(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
 
-    public async Task<IEnumerable<CategoryDTO>> GetAll()
-    {
-        IEnumerable<Category> categories = await _categoryRepository.GetAll();
-        return _mapper.Map<IEnumerable<CategoryDTO>>(categories);
+        public async Task<IEnumerable<CategoryDTO>> GetAll()
+        {
+            IEnumerable<Category> categories = await _categoryRepository.GetAll();
+            return _mapper.Map<IEnumerable<CategoryDTO>>(categories);
+        }
+
+        public async Task<CategoryDTO?> GetById(int id)
+        {
+            Category? category = await _categoryRepository.GetById(id);
+            return category != null ? _mapper.Map<CategoryDTO>(category) : null;
+        }
+
+        public async Task Add(CategoryDTO categoryDTO)
+        {
+            Category category = _mapper.Map<Category>(categoryDTO);
+            await _categoryRepository.Add(category);
+        }
+
+        public async Task Update(int id, CategoryDTO categoryDTO)
+        {
+            Category category = await _categoryRepository.GetById(id);
+
+            if (category == null)
+            {
+                throw new KeyNotFoundException($"Category with ID {id} not found.");
+            }
+            _mapper.Map(categoryDTO, category);
+
+            await _categoryRepository.Update(category);
+        }
+
+        public async Task Delete(int id)
+        {
+            await _categoryRepository.Delete(id);
+        }
     }
-}
 }
