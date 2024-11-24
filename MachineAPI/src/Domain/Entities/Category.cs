6812cad20468cedebf6c5e19c0244173b952a94e
Ana Julia M Martins <ana.martins_2004@outlook.com>
2024-11-24 19:07:56 -0300

    feat: rename label attribute name

--- MachineAPI/src/Domain/Entities/Category.cs
+++ MachineAPI/src/Domain/Entities/Category.cs
@@ -3,7 +3,7 @@ namespace MachineAPI.Domain.Entities
     public class Category
     {
         public required int Id { get; set; }
-        public required string Label { get; set; }
+        public required string Name { get; set; }
 
         // A Categry have a list of machines
         public List<Machine> Machines { get; set; } = new List<Machine>();
