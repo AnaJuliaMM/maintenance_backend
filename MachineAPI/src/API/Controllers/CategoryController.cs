906a8b3035887e954aa737cda225f7aa419bd011
Ana Julia M Martins <ana.martins_2004@outlook.com>
2024-11-24 19:10:30 -0300

    feat: add create and update and delete methods in controller

--- MachineAPI/src/API/Controllers/CategoryController.cs
+++ MachineAPI/src/API/Controllers/CategoryController.cs
@@ -1,38 +1,63 @@
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
 using MachineAPI.API.DTOs;
 using MachineAPI.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MachineAPI.API.Controllers
 {
-[ApiController]
-[Route("api/[controller]")]
-public class CategoryController : ControllerBase
-{
-    private readonly ICategoryService _categoryService;
-
-    public CategoryController(ICategoryService categoryService)
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoryController : ControllerBase
     {
-        _categoryService = categoryService;
-    }
+        private readonly ICategoryService _categoryService;
 
-    [HttpGet("{id}")]
-    public async Task<ActionResult<CategoryDTO?>> GetById(int id)
-    {
-        CategoryDTO? category = await _categoryService.GetById(id);
-        if (category == null)
+        public CategoryController(ICategoryService categoryService)
         {
-            return NotFound();
+            _categoryService = categoryService;
         }
-        return Ok(category);
-    }
 
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<CategoryDTO>>> GetAll()
-    {
-        IEnumerable<CategoryDTO> categories = await _categoryService.GetAll();
-        return Ok(categories);
-    }
-}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CategoryDTO?>> GetById(int id)
+        {
+            CategoryDTO? category = await _categoryService.GetById(id);
+            if (category == null)
+            {
+                return NotFound("Category ID does not exist");
+            }
+            return Ok(category);
+        }
 
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoryDTO>>> GetAll()
+        {
+            IEnumerable<CategoryDTO> categories = await _categoryService.GetAll();
+            return Ok(categories);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create([FromBody] CategoryDTO categoryDTO)
+        {
+            await _categoryService.Add(categoryDTO);
+            return CreatedAtAction(nameof(GetById), new { id = categoryDTO.Id }, categoryDTO);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update(int id, [FromBody] CategoryDTO categoryDTO)
+        {
+            if (id != categoryDTO.Id)
+            {
+                return BadRequest();
+            }
+
+            await _categoryService.Update(id, categoryDTO);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            await _categoryService.Delete(id);
+            return NoContent();
+        }
+    }
 }
