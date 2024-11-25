823bc018fdc52bdf332a3c51b2a94f4beec67e77
Ana Julia M Martins <ana.martins_2004@outlook.com>
2024-11-24 22:04:52 -0300

    feat(machineApi): change swagger title

--- MachineAPI/src/API/Startup.cs
+++ MachineAPI/src/API/Startup.cs
@@ -36,7 +36,7 @@ public class Startup
         services.AddSwaggerGen(c =>
         {
             c.SwaggerDoc("v1", new OpenApiInfo { 
-                Title = "Machine Authentication API", 
+                Title = "Machine API", 
                 Version = "v1" });
         });  
 
