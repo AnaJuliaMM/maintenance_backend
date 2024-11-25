7305d44026b7ee2db571d93a0046c2edae5efeb5
Ana Julia M Martins <ana.martins_2004@outlook.com>
2024-11-24 21:30:30 -0300

    feat(location): config attributes as nullable and remove default value

--- MachineAPI/src/Domain/Entities/Location.cs
+++ MachineAPI/src/Domain/Entities/Location.cs
@@ -4,10 +4,9 @@ namespace MachineAPI.Domain.Entities
     {
         public required int Id { get; set; }
         public required string Name { get; set; }
-        public string Description { get; set; } = string.Empty;
+        public string? Description { get; set; }
 
         // A location have a list of machines
         public List<Machine> Machines { get; set; } = new List<Machine>();
-
     }
 }
