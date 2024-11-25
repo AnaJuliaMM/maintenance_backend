6a6d31e8c2cd267f2fa5376d293f10c2979e4cbe
Ana Julia M Martins <ana.martins_2004@outlook.com>
2024-11-24 21:26:04 -0300

    feat(machine): update dto and entity machine nullable attributes

--- MachineAPI/src/API/DTOs/MachineDTO.cs
+++ MachineAPI/src/API/DTOs/MachineDTO.cs
@@ -2,14 +2,12 @@ namespace MachineAPI.API.DTOs
 {
     public class MachineDTO
     {
-
-        public required int Id { get; set; }
-        public int? SerialNumber { get; set; } 
+        public int Id { get; set; }
+        public int? SerialNumber { get; set; }
         public required string Name { get; set; }
         public string? Model { get; set; }
-        public DateTime ManufactureDate { get; set; }
-        public int? CategoryId { get; set; }
-        public int? LocationId { get; set; }
-
+        public DateTime? ManufactureDate { get; set; }
+        public CategoryDTO? Category { get; set; }
+        public LocationDTO? Location { get; set; }
     }
 }
