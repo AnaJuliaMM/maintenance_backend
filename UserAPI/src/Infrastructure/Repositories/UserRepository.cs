92e547d00bfc4fac9d58e48ae2e94a02f7fe8579
Ana Julia M Martins <ana.martins_2004@outlook.com>
2024-11-24 23:28:25 -0300

    refactor(user): rename methods

--- UserAPI/src/Infrastructure/Repositories/UserRepository.cs
+++ UserAPI/src/Infrastructure/Repositories/UserRepository.cs
@@ -1,8 +1,8 @@
 using Microsoft.EntityFrameworkCore;
-using UserAuth.Domain.Entities;
-using UserAuth.Domain.Interfaces;
+using UserAPI.Domain.Entities;
+using UserAPI.Domain.Interfaces;
 
-namespace UserAuth.Infrastructure.Data
+namespace UserAPI.Infrastructure.Data
 {
     public class UserRepository : IUserRepository
     {
@@ -13,29 +13,29 @@ namespace UserAuth.Infrastructure.Data
             _context = context;
         }
 
-        public async Task<IEnumerable<User>> GetAllUsers()
+        public async Task<IEnumerable<User>> GetAll()
         {
             return await _context.Users.ToListAsync();
         }
 
-        public async Task<User> GetUserById(int id)
+        public async Task<User?> GetById(int id)
         {
             return await _context.Users.FindAsync(id);
         }
 
-        public async Task AddUser(User user)
+        public async Task Add(User user)
         {
             await _context.Users.AddAsync(user);
             await _context.SaveChangesAsync();
         }
 
-        public async Task UpdateUser(User user)
+        public async Task Update(User user)
         {
             _context.Users.Update(user);
             await _context.SaveChangesAsync();
         }
 
-        public async Task DeleteUser(int id)
+        public async Task Delete(int id)
         {
             var user = await _context.Users.FindAsync(id);
             if (user != null)
@@ -45,7 +45,7 @@ namespace UserAuth.Infrastructure.Data
             }
         }
 
-        public async Task<User> FindUserByUsername(string username)
+        public async Task<User?> FindByUsername(string username)
         {
             return await _context.Users.SingleOrDefaultAsync(u => u.Username == username);
         }
