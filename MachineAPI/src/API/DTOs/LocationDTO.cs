41b171ee893f1c6a1f3f2187556c90ea4e15073e
Ana Julia M Martins <ana.martins_2004@outlook.com>
2024-11-24 21:31:52 -0300

    fix(location): rename address attributes

--- MachineAPI/src/API/DTOs/LocationDTO.cs
+++ MachineAPI/src/API/DTOs/LocationDTO.cs
@@ -3,6 +3,8 @@ namespace MachineAPI.API.DTOs
     public class LocationDTO
     {
         public int Id { get; set; }
-        public string? Address { get; set; }
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
     }
 }
