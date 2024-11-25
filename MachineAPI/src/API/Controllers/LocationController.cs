caccff19d0c75114022b6469ffe4d33c0bf0d129
Ana Julia M Martins <ana.martins_2004@outlook.com>
2024-11-24 21:32:35 -0300

    feat(location): add create and update and delete methods in controller

--- MachineAPI/src/API/Controllers/LocationController.cs
+++ MachineAPI/src/API/Controllers/LocationController.cs
@@ -1,38 +1,63 @@
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
 using MachineAPI.API.DTOs;
 using MachineAPI.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MachineAPI.API.Controllers
 {
-[ApiController]
-[Route("api/[controller]")]
-public class LocationController : ControllerBase
-{
-    private readonly ILocationService _locationService;
-
-    public LocationController(ILocationService locationService)
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LocationController : ControllerBase
     {
-        _locationService = locationService;
-    }
+        private readonly ILocationService _locationService;
 
-    [HttpGet("{id}")]
-    public async Task<ActionResult<LocationDTO?>> GetById(int id)
-    {
-        LocationDTO? location = await _locationService.GetById(id);
-        if (location == null)
+        public LocationController(ILocationService locationService)
         {
-            return NotFound();
+            _locationService = locationService;
         }
-        return Ok(location);
-    }
 
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<LocationDTO>>> GetAll()
-    {
-        IEnumerable<LocationDTO> locations = await _locationService.GetAll();
-        return Ok(locations);
-    }
-}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<LocationDTO?>> GetById(int id)
+        {
+            LocationDTO? location = await _locationService.GetById(id);
+            if (location == null)
+            {
+                return NotFound("Location ID does not exist");
+            }
+            return Ok(location);
+        }
 
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<LocationDTO>>> GetAll()
+        {
+            IEnumerable<LocationDTO> locations = await _locationService.GetAll();
+            return Ok(locations);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create([FromBody] LocationDTO locationDTO)
+        {
+            await _locationService.Add(locationDTO);
+            return CreatedAtAction(nameof(GetById), new { id = locationDTO.Id }, locationDTO);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update(int id, [FromBody] LocationDTO locationDTO)
+        {
+            if (id != locationDTO.Id)
+            {
+                return BadRequest();
+            }
+
+            await _locationService.Update(id, locationDTO);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            await _locationService.Delete(id);
+            return NoContent();
+        }
+    }
 }
