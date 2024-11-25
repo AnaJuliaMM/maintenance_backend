4c5442cf652c83277f6554b20c916ac7e0b351bb
Ana Julia M Martins <ana.martins_2004@outlook.com>
2024-11-24 21:31:34 -0300

    feat(location): implement create and delete and update methods in service

--- MachineAPI/src/Application/Services/LocationService.cs
+++ MachineAPI/src/Application/Services/LocationService.cs
@@ -1,31 +1,56 @@
 using AutoMapper;
+using MachineAPI.API.DTOs;
 using MachineAPI.Application.Interfaces;
 using MachineAPI.Domain.Entities;
 using MachineAPI.Domain.Interfaces;
-using MachineAPI.API.DTOs;
 
-namespace MachineAPI.Application.Services{
-public class LocationService : ILocationService
+namespace MachineAPI.Application.Services
 {
-    private readonly ILocationRepository _locationRepository;
-    private readonly IMapper _mapper;
-
-    public LocationService(ILocationRepository locationRepository, IMapper mapper)
+    public class LocationService : ILocationService
     {
-        _locationRepository = locationRepository;
-        _mapper = mapper;
-    }
+        private readonly ILocationRepository _locationRepository;
+        private readonly IMapper _mapper;
 
-    public async Task<LocationDTO?> GetById(int id)
-    {
-        var location = await _locationRepository.GetById(id);
-        return location != null ? _mapper.Map<LocationDTO>(location) : null;
-    }
+        public LocationService(ILocationRepository locationRepository, IMapper mapper)
+        {
+            _locationRepository = locationRepository;
+            _mapper = mapper;
+        }
 
-    public async Task<IEnumerable<LocationDTO>> GetAll()
-    {
-        var locations = await _locationRepository.GetAll();
-        return _mapper.Map<IEnumerable<LocationDTO>>(locations);
+        public async Task<IEnumerable<LocationDTO>> GetAll()
+        {
+            var locations = await _locationRepository.GetAll();
+            return _mapper.Map<IEnumerable<LocationDTO>>(locations);
+        }
+
+        public async Task<LocationDTO?> GetById(int id)
+        {
+            var location = await _locationRepository.GetById(id);
+            return location != null ? _mapper.Map<LocationDTO>(location) : null;
+        }
+
+        public async Task Add(LocationDTO locationDTO)
+        {
+            Location location = _mapper.Map<Location>(locationDTO);
+            await _locationRepository.Add(location);
+        }
+
+        public async Task Update(int id, LocationDTO locationDTO)
+        {
+            Location? location = await _locationRepository.GetById(id);
+
+            if (location == null)
+            {
+                throw new KeyNotFoundException($"Category with ID {id} not found.");
+            }
+            _mapper.Map(locationDTO, location);
+
+            await _locationRepository.Update(location);
+        }
+
+        public async Task Delete(int id)
+        {
+            await _locationRepository.Delete(id);
+        }
     }
-}
 }
