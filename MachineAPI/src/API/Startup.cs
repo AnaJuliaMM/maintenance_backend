51ba2885e241d3d211442e182771e07fb8b9b67b
Ana Julia M Martins <ana.martins_2004@outlook.com>
2024-11-27 07:41:46 -0300

    feat: allow frontend requests

--- MachineAPI/src/API/Startup.cs
+++ MachineAPI/src/API/Startup.cs
@@ -1,12 +1,13 @@
-using Microsoft.EntityFrameworkCore;
-using MachineAPI.API.Extensions;
-using MachineAPI.Infrastructure.Data;
-using MachineAPI.Domain.Interfaces;
-using Microsoft.OpenApi.Models;
-using MachineAPI.Application.Services;
-using MachineAPI.Application.Interfaces;
 using System.Text;
 using AutoMapper;
+using MachineAPI.API.Extensions;
+using MachineAPI.Application.Interfaces;
+using MachineAPI.Application.Services;
+using MachineAPI.Domain.Interfaces;
+using MachineAPI.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 public class Startup
 {
     private readonly IConfiguration _configuration;
@@ -15,10 +16,11 @@ public class Startup
     {
         _configuration = configuration;
     }
+
     public void ConfigureServices(IServiceCollection services)
     {
-        services.AddDbContext<ApplicationDbContext>(
-            options =>options.UseNpgsql(_configuration.GetConnectionString("DefaultConnection"))
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(_configuration.GetConnectionString("DefaultConnection"))
         );
 
         services.AddScoped<IMachineRepository, MachineRepository>();
@@ -31,14 +33,26 @@ public class Startup
 
         services.AddAutoMapper(typeof(MappingDTO));
 
+        services.AddCors(options =>
+        {
+            options.AddPolicy(
+                "AllowLocalhost3000",
+                builder =>
+                {
+                    builder
+                        .WithOrigins("http://localhost:3000", "http://host.docker.internal:3000")
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                }
+            );
+        });
+
         services.AddControllers();
         services.AddEndpointsApiExplorer();
         services.AddSwaggerGen(c =>
         {
-            c.SwaggerDoc("v1", new OpenApiInfo { 
-                Title = "Machine API", 
-                Version = "v1" });
-        });  
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Machine API", Version = "v1" });
+        });
 
         services.AddAuthentication();
         services.AddAuthorization();
@@ -48,19 +62,20 @@ public class Startup
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
-
         // Configure the HTTP request pipeline.
         if (env.IsDevelopment() || env.IsProduction())
         {
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
-                
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "User Authentication API V1"); // Set up the Swagger UI
                 c.RoutePrefix = string.Empty; // Set Swagger UI at the app's root
             });
             app.ApplyMigrations();
         }
+
+        // Habilitar CORS antes do roteamento
+        app.UseCors("AllowLocalhost3000");
 
         // app.UseHttpsRedirection();
 
