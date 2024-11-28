3af9fc9428ee07427bdbfc75381b269449d0b4d4
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-11-28 16:11:25 -0300

    feat: remove required from id categoryDto

--- MachineAPI/src/API/DTOs/CategoryDTO.cs
+++ MachineAPI/src/API/DTOs/CategoryDTO.cs
@@ -2,7 +2,7 @@ namespace MachineAPI.API.DTOs
 {
     public class CategoryDTO
 {
-    public required int Id { get; set; }
+    public int Id { get; set; }
     public required string Name { get; set; }
 }
 }
