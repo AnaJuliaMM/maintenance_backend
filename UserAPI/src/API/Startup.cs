35edd4198e95b32698713d408648f6de5c7e8830
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-11-29 21:44:41 -0300

    feat(user): allow cors for frontend request

--- UserAPI/src/API/Startup.cs
+++ UserAPI/src/API/Startup.cs
@@ -1,13 +1,13 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using UserAPI.API.Extensions;
 using UserAPI.Application.Interfaces;
 using UserAPI.Application.Services;
 using UserAPI.Domain.Interfaces;
 using UserAPI.Infrastructure.Data;
-using Microsoft.OpenApi.Models;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 
 public class Startup
 {
@@ -17,10 +17,11 @@ public class Startup
     {
         _configuration = configuration;
     }
+
     public void ConfigureServices(IServiceCollection services)
     {
-        services.AddDbContext<ApplicationDbContext>(
-            options =>options.UseNpgsql(_configuration.GetConnectionString("DefaultConnection"))
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(_configuration.GetConnectionString("DefaultConnection"))
         );
 
         // Todas as interfaces devem ser adicionados aqui (interface, classe que implementa)
@@ -31,58 +32,81 @@ public class Startup
 
         services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
+        services.AddCors(options =>
+        {
+            options.AddPolicy(
+                "AllowLocalhost3000",
+                builder =>
+                {
+                    builder
+                        .WithOrigins("http://localhost:3000", "http://host.docker.internal:3000")
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                }
+            );
+        });
 
         services.AddControllers();
         services.AddEndpointsApiExplorer();
         services.AddSwaggerGen(c =>
         {
-            c.SwaggerDoc("v1", new OpenApiInfo { 
-                Title = "User Authentication API", 
-                Version = "v1" });
-            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme() {
-                Name = "Authorization",
-                Type = SecuritySchemeType.ApiKey,
-                Scheme = "Bearer",
-                BearerFormat = "JWT",
-                In = ParameterLocation.Header,
-                Description = "JWT Authorization Header is using Bearer Schema " +
-                              "\n\n Input 'Bearer' + token"
-            });
-            c.AddSecurityRequirement(new OpenApiSecurityRequirement() 
-            {
+            c.SwaggerDoc(
+                "v1",
+                new OpenApiInfo { Title = "User Authentication API", Version = "v1" }
+            );
+            c.AddSecurityDefinition(
+                "Bearer",
+                new OpenApiSecurityScheme()
                 {
-                    new OpenApiSecurityScheme 
-                    {
-                        Reference = new OpenApiReference 
-                        {
-                            Type = ReferenceType.SecurityScheme,
-                            Id = "Bearer"
-                        }
-                    },
-                    Array.Empty<String>()
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description =
+                        "JWT Authorization Header is using Bearer Schema "
+                        + "\n\n Input 'Bearer' + token",
                 }
-            });
-        });  
-
-        services.AddAuthentication(options =>
-        {
-            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-        })
-        .AddJwtBearer(options =>
-        {
-            options.TokenValidationParameters = new TokenValidationParameters
-            {
-                ValidateIssuer = true,
-                ValidateAudience = true,
-                ValidateLifetime = true,
-                ValidateIssuerSigningKey = true,
-                ValidIssuer = _configuration["Jwt:Issuer"],
-                ValidAudience = _configuration["Jwt:Audience"],
-                IssuerSigningKey = new SymmetricSecurityKey(
-                    Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]))
-            };
+            );
+            c.AddSecurityRequirement(
+                new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer",
+                            },
+                        },
+                        Array.Empty<String>()
+                    },
+                }
+            );
         });
+
+        services
+            .AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = _configuration["Jwt:Issuer"],
+                    ValidAudience = _configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(_configuration["Jwt:Key"])
+                    ),
+                };
+            });
         services.AddAuthorization();
 
         services.AddSingleton<IConfiguration>(_configuration);
@@ -90,19 +114,20 @@ public class Startup
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
-
         // Configure the HTTP request pipeline.
         if (env.IsDevelopment() || env.IsProduction())
         {
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
-                
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "User Authentication API V1"); // Set up the Swagger UI
                 c.RoutePrefix = string.Empty; // Set Swagger UI at the app's root
             });
             app.ApplyMigrations();
         }
+
+        // Habilitar CORS antes do roteamento
+        app.UseCors("AllowLocalhost3000");
 
         // app.UseHttpsRedirection();
 
