86786badb3b419fb5a26d71a5ae12f1fb727b4cf
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-12-02 22:54:30 -0300

    feat(user): user primary constructor

--- UserAPI/src/API/Controllers/AuthController.cs
+++ UserAPI/src/API/Controllers/AuthController.cs
@@ -1,23 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
 using UserAPI.API.DTOs;
-using UserAPI.Application.Helpers;
 using UserAPI.Application.Interfaces;
-using UserAPI.Application.Services;
 
 namespace UserAPI.API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class AuthController : ControllerBase
+    public class AuthController(IAuthService authService, ITokenService tokenService)
+        : ControllerBase
     {
-        private readonly IAuthService _authService;
-        private readonly ITokenService _tokenService;
-
-        public AuthController(IAuthService authService, ITokenService tokenService)
-        {
-            _authService = authService;
-            _tokenService = tokenService;
-        }
+        private readonly IAuthService _authService = authService;
+        private readonly ITokenService _tokenService = tokenService;
 
         [HttpPost]
         public async Task<IActionResult> LoginUser(UserLoginDTO userLoginDTO)
