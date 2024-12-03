48c24fcf253c15b9b0422511a0576b4bed7c2d40
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-12-02 22:50:21 -0300

    feat(user): add role in db context

--- UserAPI/src/Infrastructure/Data/ApplicationDbContext.cs
+++ UserAPI/src/Infrastructure/Data/ApplicationDbContext.cs
@@ -3,22 +3,25 @@ using UserAPI.Domain.Entities;
 
 namespace UserAPI.Infrastructure.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : DbContext(options)
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options) { }
-
-        public DbSet<User> Users { get; set; }
+        public required DbSet<User> Users { get; set; }
+        public required DbSet<Role> Roles { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-    
-            modelBuilder.Entity<User>()
-                .HasIndex(u => u.Username)
-                .IsUnique(); 
+            // Create Role and User 1:n relationship
+            modelBuilder
+                .Entity<User>()
+                .HasOne(u => u.Role)
+                .WithMany(r => r.Users)
+                .HasForeignKey(u => u.RoleId);
 
+            modelBuilder.Entity<User>().HasIndex(u => u.Username).IsUnique();
+
+            // Persist relationships
             base.OnModelCreating(modelBuilder);
         }
     }
-
 }
