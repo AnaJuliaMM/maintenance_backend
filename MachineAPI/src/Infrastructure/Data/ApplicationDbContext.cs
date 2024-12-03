c0a0beb73ab9c1d17e9b9b3f514e0e76d218cab2
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-12-02 22:50:36 -0300

    docs(machine): fix comments

--- MachineAPI/src/Infrastructure/Data/ApplicationDbContext.cs
+++ MachineAPI/src/Infrastructure/Data/ApplicationDbContext.cs
@@ -1,5 +1,5 @@
-using Microsoft.EntityFrameworkCore;
 using MachineAPI.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace MachineAPI.Infrastructure.Data
 {
@@ -11,18 +11,19 @@ namespace MachineAPI.Infrastructure.Data
         public DbSet<Machine> Machines { get; set; }
         public DbSet<Location> Locations { get; set; }
         public DbSet<Category> Categories { get; set; }
-        
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {   
 
-            // Create Machine and Category 1:n relationship
-            modelBuilder.Entity<Machine>()
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Create Machine and Category n:1 relationship
+            modelBuilder
+                .Entity<Machine>()
                 .HasOne(machine => machine.Category)
                 .WithMany(category => category.Machines)
                 .HasForeignKey(machine => machine.CategoryId);
 
-            // Create Machine and Location 1:n relationship
-            modelBuilder.Entity<Machine>()
+            // Create Machine and Location n:1 relationship
+            modelBuilder
+                .Entity<Machine>()
                 .HasOne(machine => machine.Location)
                 .WithMany(location => location.Machines)
                 .HasForeignKey(machine => machine.LocationId);
@@ -30,7 +31,5 @@ namespace MachineAPI.Infrastructure.Data
             // Persist relationships
             base.OnModelCreating(modelBuilder);
         }
-
     }
-
 }
