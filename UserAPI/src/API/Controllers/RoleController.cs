dda26d0f7e92e4067f9354df26438e956f11d892
Ana Julia M Martins <ana.martins_2004@outlook.com>
2024-12-03 22:42:14 -0300

    feat(user): create role controller

---
+++ UserAPI/src/API/Controllers/RoleController.cs
@@ -0,0 +1,117 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using UserAPI.API.DTOs;
+using UserAPI.Application.Interfaces;
+
+namespace roleAPI.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    // [Authorize(Roles = "role:admin")]
+    public class RoleController(IRoleService roleService) : ControllerBase
+    {
+        private readonly IRoleService _roleService = roleService;
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            try
+            {
+                RoleDTO? role = await _roleService.GetById(id);
+                return Ok(role);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest($"Erro de argumento: {ex.Message}");
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Erro interno do servidor: {ex.Message}");
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                IEnumerable<RoleDTO> roles = await _roleService.GetAll();
+                return Ok(roles);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Erro interno do servidor: {ex.Message}");
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(RoleDTO roleDTO)
+        {
+            try
+            {
+                await _roleService.Add(roleDTO);
+                return CreatedAtAction(nameof(GetById), new { id = roleDTO.Id }, roleDTO);
+            }
+            catch (ArgumentNullException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Erro interno do servidor: {ex.Message}");
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, RoleDTO roleDTO)
+        {
+            try
+            {
+                await _roleService.Update(id, roleDTO);
+                return NoContent();
+            }
+            catch (ArgumentNullException ex)
+            {
+                return BadRequest($"Erro de argumento: {ex.Message}");
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest($"Erro de argumento: {ex.Message}");
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Erro interno do servidor: {ex.Message}");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                await _roleService.Delete(id);
+                return NoContent();
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest($"Erro de argumento: {ex.Message}");
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Erro interno do servidor: {ex.Message}");
+            }
+        }
+    }
+}
