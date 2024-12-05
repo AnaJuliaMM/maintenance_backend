08f5cd90cc4449b4b56e5f3486360e13b634563f
Ana Julia M Martins <ana.martins_2004@outlook.com>
2024-12-05 07:53:14 -0300

    feat(gateway): config ocelot service and swagger

--- GatewayAPI/src/Program.cs
+++ GatewayAPI/src/Program.cs
@@ -1,52 +1,112 @@
-using Ocelot.DependencyInjection;
-using Ocelot.Middleware;
+using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
-using System.Text;
+using Microsoft.OpenApi.Models;
+using MMLib.SwaggerForOcelot.DependencyInjection;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Authentication
-builder.Services.AddAuthentication(options =>
+// Config Authentication
+builder
+    .Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
         {
-            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-        })
-        .AddJwtBearer(options =>
-        {
-            options.TokenValidationParameters = new TokenValidationParameters
-            {
-                ValidateIssuer = true,
-                ValidateAudience = true,
-                ValidateLifetime = true,
-                ValidateIssuerSigningKey = true,
-                ValidIssuer = "issuer",
-                ValidAudience = "audience",
-                IssuerSigningKey = new SymmetricSecurityKey(
-                    Encoding.UTF8.GetBytes("my_secret_key"))
-            };
-        });
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = "issuer",
+            ValidAudience = "audience",
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes("ewgvgwvxiqboqxbuoqnx927et632fev287bh")
+            ),
+        };
+    });
+
+// Add Ocelot e Swagger
+builder.Services.AddOcelot();
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
-// Working with Ocelot
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// Swagger API Gateway
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc(
+        "v1",
+        new OpenApiInfo
+        {
+            Title = "API Gateway",
+            Version = "v1",
+            Description = "API Gateway para rotear requisições para as APIs downstream",
+        }
+    );
+
+    // Configurações adicionais podem ser adicionadas aqui, como esquemas de segurança, se necessário.
+    c.AddSecurityDefinition(
+        "Bearer",
+        new OpenApiSecurityScheme
+        {
+            In = ParameterLocation.Header,
+            Description = "Insira o token JWT",
+            Name = "Authorization",
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer",
+        }
+    );
+    c.AddSecurityRequirement(
+        new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer",
+                    },
+                },
+                new string[] { }
+            },
+        }
+    );
+});
 
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
-
+// Enable authentication and authorization
 app.UseAuthentication();
 app.UseAuthorization();
 
+// Swagger UI
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "API Gateway");
+    c.SwaggerEndpoint("http://localhost:3011/swagger/v1/swagger.json", "User API");
+    c.SwaggerEndpoint("http://localhost:3012/swagger/v1/swagger.json", "Machine API");
+    c.SwaggerEndpoint("http://localhost:3013/swagger/v1/swagger.json", "Warehouse API");
+    c.RoutePrefix = string.Empty;
+});
 
-// Configurate app to use ocelot, that is the middleware that redirect 
+// SwaggerForOcelot UI
+app.UseSwaggerForOcelotUI(options =>
+{
+    options.PathToSwaggerGenerator = "/swagger/docs";
+});
+
+// Configurate app to use ocelot, that is the middleware that redirect
 //      the gateway requisition to the correspondent microsservice
-await app.UseOcelot(); 
+await app.UseOcelot();
 
 app.Run();
